86ef0b1334178cad0bfb1ca33da493489791c14c
DanielPetk <danielpetkoski@hotmail.com>
2025-09-21 01:22:53 -0400

    ughh

--- client/client.cpp
+++ client/client.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h> 
-#include <windows.h>
+
+#if defined(_WIN32)
+    #include <winsock2.h>
+    #include <ws2tcpip.h> 
+    #include <windows.h>
+#else
+    #include <array>
+#endif
 
 #include "ClientApp.h"
 
 int main() {
 
+#if defined(_WIN32)
     WSADATA wsaData;
     int result = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (result != 0){
         std::cerr << "Failed to init wsaData";
         std::exit(1);
     }
+#endif
 
     ClientApp clientApp;
     clientApp.Run();
 
+#if defined(_WIN32)
     WSACleanup();
+#endif
 }
